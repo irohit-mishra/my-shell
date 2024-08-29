028a40c1deafe8055b499877f904222da3a97b60
Rohit Mishra <iamrohit330@gmail.com>
2024-08-29 10:35:46 +0530

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,6 +11,9 @@ public class Main {
         while (true) {
             if (input.equals("exit 0")) {  // Check if the input is "exit 0"
                 System.exit(0);  // Exit with status code 0
+        }else if(input.startsWith("echo ")){
+            String toEcho =input.substring(5);
+            System.out.println(toEcho);
             } else {
                 System.out.print(input + ": command not found\n");  // Print error message
                 System.out.print("$ ");  // Print prompt for the next command
